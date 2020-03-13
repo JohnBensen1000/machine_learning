4478aa6224ff7b408e8473ee3d3879c739e7e3b3
JohnBensen1000 <56515283+JohnBensen1000@users.noreply.github.com>
2020-03-12 20:40:30 -0400

    Add files via upload

---
+++ deepLearning-control.py
@@ -0,0 +1,98 @@
+from deepLearning import Neural_Network
+import numpy as np # helps with the math
+import statistics
+
+num_hid = int(input('Number of hidden layers: '))
+num_neu = int(input('Number of neurons per hidden layer: '))
+
+print('\n')
+
+num_in = int(input('Number of input data points: '))
+max_in = int(input('Maximum possible input value: '))
+num_out = int(input('Number of output data points: '))
+
+Neural_Net = Neural_Network(num_in, num_hid, num_neu, num_out)
+ 
+#####################################################################
+    
+def train_network(train_file, values_file):
+    write_to = open(values_file, 'w') 
+    raw_data = open(train_file, 'r')
+    raw_data = raw_data.readlines()
+
+    epoch = int(input('\nepoch: '))
+        
+    for i in range(1, epoch):
+        in_list = raw_data[i].split(',')[1:num_in + 1]
+        in_list = np.array([[float(a) / max_in] for a in in_list])         #turns data points into array of arrays--each insidea array is one float value from 0 to 1
+        
+        out_correct = [[0.0]] * num_out
+        out_correct[int(raw_data[i].split(',')[0])] = [1.0]
+                
+        Neural_Net.feed_forward(in_list, out_correct)
+        Neural_Net.back_prop(out_correct, in_list, i) 
+
+        if((i % 1000) == 0): 
+            print(str(i) + '           ' + str((Neural_Net.costFunc / i)) ) 
+
+def find_accuracy(test_file):
+    epoch = int(input('\nepoch: '))
+    
+    test_file = open(test_file, 'r')
+    test_data = test_file.readlines()
+     
+    correct_count = 0
+
+    for i in range(1, epoch):
+        in_list = test_data[i].split(',')[1:num_in + 1]
+        in_list = np.array([[float(a) / max_in] for a in in_list])         #turns data points into array of arrays--each insidea array is one float value from 0 to 1
+        
+        out_array = []
+        out_correct = [0]  #This array wont be used, just a place holder
+        
+        Neural_Net.feed_forward(in_list, out_correct, findCorrect=False)
+        if Neural_Net.outList.argmax() == int(test_data[i].split(',')[0]): correct_count += 1 
+            
+    print('------------------------------------')    
+    print('accuracy = ' + str(correct_count / i))
+    print('------------------------------------')  
+    
+def test_lines(test_file):
+    test_file = open(test_file, 'r')
+    test_data = test_file.readlines()
+    
+    test_line = ''
+    
+    while test_line != 'stop':
+        test_line = int(input(" (Type 'stop' to stop testing) --- Test line: "))
+        in_list = test_data[test_line].split(',')[1:num_in + 1]
+        in_list = np.array([[float(a) / max_in] for a in in_list])         #turns data points into array of arrays--each insidea array is one float value from 0 to 1
+        
+        out_array = []
+        out_correct = [0]  #This array wont be used, just a place holder
+        
+        Neural_Net.feed_forward(in_list, out_correct, findCorrect=False)
+        out_array.append( Neural_Net.outList.argmax() )
+        
+        try:                
+            print(statistics.mode(out_array))
+        except:
+            print("No result")
+        
+train_file  = input('Type train file name (should be excel file) ')
+test_file   = input('Type test file name (should be excel file) ')
+
+train_file  = 'C:/Users/bense/Downloads/training_set.csv'
+test_file   = 'C:/Users/bense/Downloads/mnist_test.csv'
+values_file = 'C:/Users/bense/OneDrive/Desktop/vanilla_learning/weights_biases.txt'
+
+while 1 == 1:
+    user_decide = input("Decide what to do ('train', 'find accuracy', 'test lines'): ")
+    
+    if(user_decide == 'train'): train_network(train_file, values_file)
+        
+    elif(user_decide == 'find accuracy'): find_accuracy(test_file)
+    
+    elif(user_decide == 'test lines'): test_lines(test_file)
+    
+    else: print("typo\n")
---
+++ deepLearning.py
@@ -0,0 +1,82 @@
+#This is a Neural Network. The design was based heavily on the video series, by 3blue1brown, that explained how a neural network works. 
+#There were several articals on towardsdatascience.com that were also very useful
+#The program asks the operator for the number of hidden layers and number of neurons in each hidden layer
+#The program also asks for the number of neural networks to train. To increase accuracy, multiple neural networks are trained, when
+#predicting the answer, the results from these networks are compared. Whichever answer shows up the most is recored as being the predicted answer
+#all input values are converted from a value from 0 to 1, all values are divided by max value
+#This also uses the sigmoid function as the activation function
+
+#training data has to be in the following format:
+    # 5,0,0,0,1,24,255,...,45,23,0,0,0
+    # First number represents the correct answer, the following numbers represent the data points
+    # Should be excel file
+    
+import random
+import numpy as np # helps with the math
+import statistics
+from numpy import dot, exp, multiply, add
+
+class Neural_Network:
+    def __init__(self, numIn, numHid, numNeu, numOut):
+        #number of activation layers = number of hidden plus output layer
+        self.numLayers = numHid + 1                                                             
+        self.numOut    = numOut
+        #initializes random weights/biases between input layer and first hidden layer
+        self.wList, self.bList = [np.random.randn(numNeu, numIn)], [np.zeros((numNeu, 1))]    
+        #initializes random weights/biases between hidden layers and output layer
+        for i in range(numHid - 1):                                    
+            self.wList.append(np.random.randn(numNeu, numNeu))   
+            self.bList.append(np.zeros((numNeu, 1)))
+        #initializes weights/biases between last hidden layer and output layer
+        self.wList.append(np.random.randn(numOut, numNeu))           
+        self.bList.append(np.zeros((numOut, 1)))
+        #initialize learning rate
+        self.learnRate = 1.25
+        self.costFunc  = 0
+        
+    def activation(self, z, deriv=False):         
+         #activation function ==> S(x) = 1/1+e^(-x), derivative of activation function ==> z * (1 - z)
+        if deriv == True:
+            return z * (1 - z)
+        return 1 / (1 + exp(-z))
+        
+    def feed_forward(self, inList, outCorrect, findCorrect=True):                                   
+        #finds activations for each hidden layer and output layer
+        self.aList = [self.activation(dot( self.wList[0], inList ) + self.bList[0], False)]  
+        #goes through each hidden layer plus the final layer     
+        for a in range(1, self.numLayers):
+            self.aList.append(self.activation(dot( self.wList[a], self.aList[a - 1] ) + self.bList[a]))    
+        #output layer set to last layer in aList          
+        self.outList = self.aList[a]    
+        #keeps track of cost function 
+        if(findCorrect == True):
+            for i in range(self.numOut): self.costFunc += (((self.outList[i] - outCorrect[i]) ** 2) / self.numOut)   
+              
+    def back_prop_layer(self, a, tempA0, tempDA1, adjustPre=True ):
+        dz = self.activation(self.aList[a], deriv=True)
+        #adjusts format of a2_list so it could be used          
+        tempA0 = tempA0.flatten()                                                       
+        
+        tempDA0 = np.array( [0.0] * len(tempA0) )                                       
+        if(adjustPre == True): 
+            #finds total adjustments that should be made for previous activation, does not perform this task for input layer
+            for i in range(len(self.aList[a])): 
+                tempDA0 = add(tempDA0, (tempDA1[i] * dz[i]) * self.wList[a][i])      
+        #adjustments made to weights/biases based on gradient descent                                                                                           
+        self.wList[a] -= multiply([(i * tempA0) for i in (tempDA1.flatten() * dz.flatten())], self.learnRate)     
+        self.bList[a] -= multiply([[i] for i in (tempDA1.flatten() * dz.flatten())]         , self.learnRate)
+        
+        return multiply(tempDA0, self.learnRate)              
+            
+    def back_prop(self, outCorrect, inList, count): 
+        #starts at output layer and works backwards, adjusts all weights/biases for single test case
+        daList = [(self.outList - outCorrect).flatten()]
+        
+        for a in range(self.numLayers - 1, 0, -1): 
+            #da = a - a_correct, change in previos activation should be the difference between what the activation is and what it should be,       
+            daList.append( self.back_prop_layer(a, self.aList[a - 1], daList[self.numLayers - 1 - a]) )
+        self.back_prop_layer(0, inList, daList[self.numLayers - 1 - 0], adjustPre=False)   
+        
+        learnCount = (count % 5000) / 5000
+        self.learnRate = 1.25 - learnCount
+        
